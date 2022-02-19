44471946dd553b7e5b214a53f1c8ea2777e3b8c7
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-02-19 19:42:54 +0200

    add tests folder with junit4 headless runner

--- core/src/com/tiem625/break4j/Break4jGameLauncher.java
+++ core/src/com/tiem625/break4j/Break4jGameLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Break4jGameMainLoop extends ApplicationAdapter {
+public class Break4jGameLauncher extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
