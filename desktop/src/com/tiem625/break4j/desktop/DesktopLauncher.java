0a4cd47aa459ae2bc403a09725b21eab4375e423
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-02-19 19:03:25 +0200

    first commit empty gdx project

---
+++ desktop/src/com/tiem625/break4j/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tiem625.break4j.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tiem625.break4j.Break4jGameMainLoop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Break4jGameMainLoop(), config);
+	}
+}
