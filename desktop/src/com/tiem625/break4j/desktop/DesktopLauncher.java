1d953849ca0e26cefd56d6415c9e3029d7a2142a
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-02-21 17:33:00 +0200

    rename gameloop classes

--- desktop/src/com/tiem625/break4j/desktop/DesktopLauncher.java
+++ desktop/src/com/tiem625/break4j/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.tiem625.break4j.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.tiem625.break4j.Break4jGameLauncher;
+import com.tiem625.break4j.Break4jGameLoop;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Break4jGameLauncher(), config);
+		new LwjglApplication(new Break4jGameLoop(), config);
 	}
 }
