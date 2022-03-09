489dedd600ab7de84bde585c3339ce44a8696ee3
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-03-09 18:46:45 +0200

    WIP redo render grid as builder

--- core/src/com/tiem625/break4j/gdx/grid/BricksGridGdxRender.java
+++ core/src/com/tiem625/break4j/gdx/grid/BricksGridGdxRender.java
@@ -4,13 +4,43 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.tiem625.break4j.ScreenPosition;
 import com.tiem625.break4j.model.grid.BricksGrid;
 
+import static com.tiem625.break4j.tools.Verifiers.verifiedNotNull;
+
 public class BricksGridGdxRender extends Group {
 
     private final BricksGrid model;
 
-    public BricksGridGdxRender(BricksGrid model, ScreenPosition position) {
+    private BricksGridGdxRender(BricksGrid model, ScreenPosition position) {
 
         this.model = model;
         this.setPosition(position.getX(), position.getY());
+    }
+
+    public static BricksGridRendering forModel(BricksGrid model) {
+        return new BricksGridRendering(model);
+    }
+
+    public static class BricksGridRendering {
+
+        private final BricksGrid model;
+        private ScreenPosition gridPosition;
+        private int bricksVerticalGap;
+        private int bricksHorizontalGap;
+
+        private BricksGridRendering(BricksGrid model) {
+            this.model = verifiedNotNull(model);
+            this.gridPosition = ScreenPosition.ORIGIN;
+            this.bricksVerticalGap = 0;
+            this.bricksHorizontalGap = 0;
+        }
+
+        public BricksGridGdxRender render() {
+            throw new UnsupportedOperationException("TODO");
+        }
+
+        public BricksGridRendering atPosition(ScreenPosition screenPosition) {
+            this.gridPosition = screenPosition;
+            return this;
+        }
     }
 }
--- tests/src/com/tiem625/break4j/gdx/grid/BricksGridGdxRenderTests.java
+++ tests/src/com/tiem625/break4j/gdx/grid/BricksGridGdxRenderTests.java
@@ -1,14 +1,17 @@
 package com.tiem625.break4j.gdx.grid;
 
 import com.tiem625.break4j.ScreenPosition;
+import com.tiem625.break4j.model.bricks.SimpleBrick;
 import com.tiem625.break4j.model.grid.BricksGrid;
 import com.tiem625.break4j.model.grid.GridDimensions;
+import com.tiem625.break4j.model.grid.GridPosition;
 import com.tiem625.break4j.testhelp.GdxHeadlessSupport;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -17,11 +20,35 @@ public class BricksGridGdxRenderTests {
 
     @Test
     public void create_grid_render_no_grid_model_not_allowed() {
-        assertThrows(IllegalArgumentException.class, () -> new BricksGridGdxRender(null, ScreenPosition.ORIGIN));
+        assertThrows(IllegalArgumentException.class, () -> BricksGridGdxRender.forModel(null));
     }
 
     @Test
-    public void create_grid_render_no_position_not_allowed() {
-        assertThrows(IllegalArgumentException.class, () -> new BricksGridGdxRender(new BricksGrid(GridDimensions.COLLAPSED), null));
+    public void create_grid_render_no_set_position_uses_origin() {
+        BricksGridGdxRender render = BricksGridGdxRender.forModel(new BricksGrid(GridDimensions.COLLAPSED))
+                .render();
+
+        assertEquals(0f, render.getX());
+        assertEquals(0f, render.getY());
+    }
+
+    @Test
+    public void create_grid_render_no_set_gaps_default_0() {
+        BricksGridGdxRender render = BricksGridGdxRender.forModel(new BricksGrid(GridDimensions.COLLAPSED))
+                .atPosition(ScreenPosition.at(100, 100))
+                .render();
+
+        assertEquals(0, render.getVerticalGap());
+        assertEquals(0, render.getHorizontalGap());
+    }
+
+    @Test
+    public void create_grid_render_model_has_bricks_render_has_same_bricks() {
+
+        var modelGrid = new BricksGrid(GridDimensions.rowsAndCols(1, 1));
+        modelGrid.setBrick(GridPosition.ORIGIN, new SimpleBrick());
+
+        var gridRender = new BricksGridGdxRender(modelGrid, ScreenPosition.ORIGIN);
+        assertEquals(modelGrid.getCurrentNumBricks(), gridRender.getCurrentBricksLandscape().size());
     }
 }
