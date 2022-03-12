dd84bd9e7999711f100452de6b1340ac2b45a3f9
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-03-12 10:51:45 +0200

    remove unintended behavior tests

--- tests/src/com/tiem625/break4j/gdx/grid/BricksGridGdxRenderTests.java
+++ tests/src/com/tiem625/break4j/gdx/grid/BricksGridGdxRenderTests.java
@@ -65,29 +65,4 @@ public class BricksGridGdxRenderTests {
         var gridRender = BricksGridGdxRender.forModel(modelGrid).render();
         assertEquals(modelGrid.getCurrentNumBricks(), gridRender.getCurrentBricksLandscape().size());
     }
-
-    @Test
-    public void create_grid_render_model_add_bricks_later_render_has_new_bricks() {
-
-        var modelGrid = new BricksGrid(GridDimensions.rowsAndCols(1, 2));
-        modelGrid.setBrick(GridPosition.ORIGIN, new SimpleBrick());
-        var gridRender = BricksGridGdxRender.forModel(modelGrid).render();
-        modelGrid.setBrick(GridPosition.atGridOffset(0, 1), new SimpleBrick());
-
-        assertEquals(2, gridRender.getCurrentBricksLandscape().size());
-    }
-
-    @Test
-    public void create_grid_render_model_remove_brick_later_render_also_removes_brick() {
-
-        var modelGrid = new BricksGrid(GridDimensions.rowsAndCols(1, 2));
-        modelGrid.setBrick(GridPosition.atGridOffset(0, 0), new SimpleBrick());
-        modelGrid.setBrick(GridPosition.atGridOffset(0, 1), new SimpleBrick());
-
-        var gridRender = BricksGridGdxRender.forModel(modelGrid).render();
-        modelGrid.removeBrick(GridPosition.atGridOffset(0, 0));
-        modelGrid.removeBrick(GridPosition.atGridOffset(0, 1));
-
-        assertEquals(0, gridRender.getCurrentBricksLandscape().size());
-    }
 }
