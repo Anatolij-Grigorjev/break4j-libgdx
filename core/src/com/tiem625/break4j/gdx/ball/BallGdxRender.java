c03032be51978bacfd804f7c6d16d2e81c8859ff
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-03-25 18:30:19 +0200

    act ball rendere

--- core/src/com/tiem625/break4j/gdx/ball/BallGdxRender.java
+++ core/src/com/tiem625/break4j/gdx/ball/BallGdxRender.java
@@ -1,6 +1,7 @@
 package com.tiem625.break4j.gdx.ball;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.tiem625.break4j.model.ball.Ball;
 import com.tiem625.break4j.tools.AssetsLoader;
@@ -17,5 +18,13 @@ public class BallGdxRender extends Actor {
         this.ballTexture = AssetsLoader
                 .loadInternalDisposable(BALL_TEXTURE_PATH, Texture::new)
                 .orElseThrow(IllegalStateException::new);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        var currentPosition = new Vector2(getX(), getY());
+        var newPosition = currentPosition.mulAdd(model.velocity().asVector(), delta);
+        setPosition(newPosition.x, newPosition.y);
     }
 }
