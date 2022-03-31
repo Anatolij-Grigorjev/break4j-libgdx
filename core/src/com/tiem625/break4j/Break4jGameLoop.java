c3009fb6f60845b24da5fb7b201946c488d10787
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2022-03-31 18:29:44 +0300

    setup ortho camera

--- core/src/com/tiem625/break4j/Break4jGameLoop.java
+++ core/src/com/tiem625/break4j/Break4jGameLoop.java
@@ -2,6 +2,7 @@ package com.tiem625.break4j;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Logger;
@@ -22,13 +23,17 @@ public class Break4jGameLoop extends ApplicationAdapter {
 
     private final static Logger logger = new Logger("MAIN-LOOP", Logger.DEBUG);
 
+    OrthographicCamera camera;
     SpriteBatch batch;
     BricksGridGdxRender gridGdxRender;
     BallGdxRender ballGdxRender;
 
     @Override
     public void create() {
+        camera = new OrthographicCamera(640, 480);
+        camera.setToOrtho(false);
         batch = new SpriteBatch();
+
         BricksGrid model = new BricksGrid(GridDimensions.rowsAndCols(3, 3));
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -47,7 +52,14 @@ public class Break4jGameLoop extends ApplicationAdapter {
     }
 
     @Override
+    public void resize(int width, int height) {
+        camera.setToOrtho(false, width, height);
+        camera.update();
+    }
+
+    @Override
     public void render() {
+        camera.update();
         ballGdxRender.act(Gdx.graphics.getDeltaTime());
         gridGdxRender.getCurrentBricksLandscape().stream()
                 .forEach(brickGdxRender -> {
@@ -58,6 +70,7 @@ public class Break4jGameLoop extends ApplicationAdapter {
                     });
                 });
         ScreenUtils.clear(0, 0, 0, 1);
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         gridGdxRender.draw(batch, 1.0f);
         ballGdxRender.draw(batch, 1.0f);
